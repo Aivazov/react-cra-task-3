23f0cc3ce280c5e0e0301955507cf190cd3d47c0
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2022-12-11 15:30:32 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import './App.css';
 import Lecture_1 from './Lecture 1/Lecture_1';
 
 class App extends Component {
-  componentDidMount() {
-    console.log('Did Mount');
+  state = {
+    element: 'asdfas',
+  };
+
+  usingElement = () => {
+    this.setState(prev => ({element: prev.element + 1}));
   }
 
-  componentDidUpdate() {
+  componentDidMount() {
+    console.log('Did Mount');
+  };
+
+  componentDidUpdate(prevProps, prevState) {
     console.log('didUpdate');
-  }
+
+    if (this.state !== prevState.state) {
+      console.log('state object has been updated')
+    }
+  };
+
+  compoe
 
   render() {
     return (
       <div className="App">
-        <Lecture_1 />
+        <Lecture_1 state={this.state} method={this.usingElement} />
       </div>
     );
   }
--- src/Lecture 1/Lecture_1.jsx
+++ src/Lecture 1/Lecture_1.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-class Lecture_1 extends React.Component {
-  render() {
-    return <div>Hello</div>;
-  }
+function Lecture_1({state, method}) {
+  return (
+    <div>
+      <p></p>
+      <button type="button" onClick={method}>
+        {state.element}
+      </button>
+    </div>
+  );
 }
 
 export default Lecture_1;
