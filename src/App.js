a0d554a2f7c6fda424538379949b5b15765a5b1f
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2022-12-11 14:58:39 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Lecture_1 } from './Lecture 1/Lecture_1';
+
+function App() {
+  return (
+    <div className="App">
+      <Lecture_1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Lecture 1/Lecture_1.jsx
@@ -0,0 +1,4 @@
+import { Component } from "react";
+
+
+const Lecture_1 
