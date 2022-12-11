dc187884a7816474b09462def536e281d2d4503f
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2022-12-11 15:44:03 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -5,30 +5,60 @@ import Lecture_1 from './Lecture 1/Lecture_1';
 
 class App extends Component {
   state = {
-    element: 'asdfas',
+    name: 'asdfas',
+    text: '',
+  };
+
+  addElement = (e) => {
+    e.preventDefault();
+    this.setState((prev) => ({
+      text: e.currentTarget.value,
+    }));
+  };
+
+  controledSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
   };
 
   usingElement = () => {
-    this.setState(prev => ({element: prev.element + 1}));
-  }
+    this.setState((prev) => ({ element: prev.element + 1 }));
+  };
 
   componentDidMount() {
     console.log('Did Mount');
-  };
+  }
 
   componentDidUpdate(prevProps, prevState) {
     console.log('didUpdate');
 
     if (this.state !== prevState.state) {
-      console.log('state object has been updated')
+      console.log('state object has been updated');
     }
-  };
 
-  compoe
+    //lets add our state Obj to the local storage
+
+    localStorage.setItem('state', JSON.stringify(this.state.element));
+  }
+
+  compoe;
 
   render() {
+
+    console.log(this.state)
     return (
       <div className="App">
+        <form onSubmit={this.addElement}>
+          <label>
+            <input type="text" onChange={this.controledSubmit} />
+          </label>
+          <label>
+            <input type="submit" />
+          </label>
+        </form>
         <Lecture_1 state={this.state} method={this.usingElement} />
       </div>
     );
