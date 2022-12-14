a2c11026002d9c28bd53dba138e446541c5e96d5
Tim <37597659+Aivazov@users.noreply.github.com>
2022-12-15 01:27:58 +0200

    commit localStorage using

--- src/Module 3 Lecture Lifecycle 2/Reader/Reader.js
+++ src/Module 3 Lecture Lifecycle 2/Reader/Reader.js
@@ -4,10 +4,26 @@ import PropTypes from 'prop-types';
 import { Progress } from "./Progress";
 // import readingContent from "./reader.json";
 
+const LOCAL_STORAGE = 'reader_item_index';
+
 export default class Reader extends Component {
   state = {
     publicationIndex: 0
   } 
+
+  componentDidMount() {
+    const savedIndex = localStorage.getItem(LOCAL_STORAGE);
+
+    //condition is needed to check if the saveIndex not null
+    if (savedIndex) {
+      this.setState({publicationIndex: Number(savedIndex)})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log('update')
+    localStorage.setItem(LOCAL_STORAGE, this.state.publicationIndex)
+  }
 
   // onPrev = () => {
   //   this.setState(state => ({publicationIndex: state.publicationIndex - 1}))
@@ -29,14 +45,17 @@ export default class Reader extends Component {
     const {publicationIndex} = this.state;
     const {items} = this.props;
     const currentItem = this.props.items[this.state.publicationIndex];
+    const firstItem = publicationIndex === 0;
+    const lastItem_1 = publicationIndex + 1 >= items.length;
+    const lastItem_2 = publicationIndex === items.length - 1;
 
     // console.log(this.props.items[this.state.publicationIndex])
 
     return (
       <div>
         <section>
-          <button type="button" disabled={publicationIndex === 0} onClick={() => this.changeIndex(-1)}>Previous</button>
-          <button type="button" disabled={publicationIndex + 1 >= items.length} onClick={() => this.changeIndex(1)}>Next</button> 
+          <button type="button" disabled={firstItem} onClick={() => this.changeIndex(-1)}>Previous</button>
+          <button type="button" disabled={lastItem_2} onClick={() => this.changeIndex(1)}>Next</button> 
         </section>
 
         <Progress currentIdx={publicationIndex + 1} itemsLength={items.length}/>
