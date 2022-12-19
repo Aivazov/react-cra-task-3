90f3a21cb7148a44be107a9d4c659a35eb148278
Tim <37597659+Aivazov@users.noreply.github.com>
2022-12-19 16:49:52 +0200

    commit last lecture of module 3

--- src/Module_3/REST API HTTP/REST API Using mockAPI/components/Material/Material.jsx
+++ src/Module_3/REST API HTTP/REST API Using mockAPI/components/Material/Material.jsx
@@ -1,30 +1,73 @@
 import '../../Materials/Materials.css';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { IoMdAddCircleOutline } from 'react-icons/io';
+import { Component } from 'react';
 
-export const Material = ({ item, onUpdate, onDelete }) => {
+const EditMaterialModal = ({ onClose, onUpdate }) => {
   return (
     <div>
-      <p>
-        <b>Name:</b> {item.name}
-      </p>
-      <p>
-        <b>Link:</b> {item.avatar}
-      </p>
+      <p>This is a modal</p>
       <button
         type="button"
-        className="materials-buttons"
-        onClick={() => onUpdate({ id: item.id, name: Date.now() })}>
-        <IoMdAddCircleOutline />
-        Edit Item
+        onClick={() => {
+          onUpdate();
+          onClose();
+        }}>
+        Edit
       </button>
-      <button
-        type="button"
-        className="materials-buttons"
-        onClick={() => onDelete(item.id)}>
-        <RiDeleteBin6Line />
-        Remove Item
+      <button type="button" onClick={onClose}>
+        Close
       </button>
     </div>
   );
 };
+
+export class Material extends Component {
+  state = {
+    isModalOpen: false,
+  };
+
+  openModal = () => {
+    this.setState({ isModalOpen: true });
+  };
+  closeModal = () => {
+    this.setState({ isModalOpen: false });
+  };
+
+  render() {
+    const { item, onUpdate, onDelete } = this.props;
+    const { isModalOpen } = this.state;
+
+    return (
+      <div>
+        <p>
+          <b>Name:</b> {item.name}
+        </p>
+        <p>
+          <b>Link:</b> {item.avatar}
+        </p>
+        <button
+          type="button"
+          className="materials-buttons"
+          // onClick={() => onUpdate({ id: item.id, name: Date.now() })}>
+          onClick={this.openModal}>
+          <IoMdAddCircleOutline />
+          Edit Item
+        </button>
+        <button
+          type="button"
+          className="materials-buttons"
+          onClick={() => onDelete(item.id)}>
+          <RiDeleteBin6Line />
+          Remove Item
+        </button>
+        {isModalOpen && (
+          <EditMaterialModal
+            onClose={this.closeModal}
+            onUpdate={() => onUpdate({ id: item.id, name: Date.now() })}
+          />
+        )}
+      </div>
+    );
+  }
+}
