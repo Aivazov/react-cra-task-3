5c6554df8f8bc32cbd76bded82d9c903c32f5608
Tim <37597659+Aivazov@users.noreply.github.com>
2022-12-20 13:17:49 +0200

    c

--- src/Module_3/ImageFinder/components/ImageGallery/ImageGallery.js
+++ src/Module_3/ImageFinder/components/ImageGallery/ImageGallery.js
@@ -1,13 +1,13 @@
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 
 export const ImageGallery = (items) => {
-  console.log(items.items)
+  const array = items.items;
   return (
     <ul className="gallery">
       {/* <ImageGalleryItem images={items} /> */}
-      {/* {items.items.map((item) => (
+      {array?.map((item) => (
         <li key={item.id}>{item.previewURL}</li>
-      ))} */}
+      ))}
     </ul>
   );
 };
